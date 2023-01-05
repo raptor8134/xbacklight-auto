ff0b41da9145f1b7ec5090487df37c1c89f61918
James Notley <jamesnotley@gmail.com>
2023-01-05 12:35:35 -0800

    Formatting

--- camera.h
+++ camera.h
@@ -1,15 +1,15 @@
-#ifndef _MYCAMERA
-#define _MYCAMERA	1
+#ifndef __XBA_CAMERA_H__
+#define __XBA_CAMERA_H__
 
 struct getbuf_return {
-	uint8_t	*buffer;
-	int 	code;
-	int 	length;
+    uint8_t *buffer;
+    int code;
+    int length;
 };
 
-extern int camera_init(char* device, int PXWIDTH, int PXHEIGHT);
+extern int camera_init(char *device, int PXWIDTH, int PXHEIGHT);
 extern int camera_close(int fd);
 
 extern struct getbuf_return get_buffer(int fd);
 
-#endif
+#endif //__XBA_CAMERA_H__
--- main.c
+++ main.c
@@ -1,55 +1,55 @@
-#include <stdio.h>				//
-#include <stdlib.h>				//
-#include <sys/ioctl.h>			// interface with V4L2 
-#include <getopt.h>				// get command line options
-#include <fcntl.h>				// open() function
-#include <errno.h>				// Error handling
-#include <sys/mman.h>			// mmap() function
-#include <unistd.h>				// sleep() function 
-#include <stdint.h>				// uint8_t for the buffer
+#include <errno.h>      // Error handling
+#include <fcntl.h>      // open() function
+#include <getopt.h>     // get command line options
+#include <stdint.h>     // uint8_t for the buffer
+#include <stdio.h>      //
+#include <stdlib.h>     //
+#include <sys/ioctl.h>  // interface with V4L2
+#include <sys/mman.h>   // mmap() function
+#include <unistd.h>     // sleep() function
 
-#include <linux/videodev2.h>	// The main V4L2 header (camera api functions)
-#include "camera.h"				// custom camera functions to make code cleaner
+#include "camera.h"     // custom camera functions to make code cleaner
 
-#define PXWIDTH 320				// Capture resolution, change this to match the output 
-#define PXHEIGHT 240			// of `v4l2-ctl -d <n> --all | grep Bounds` if it fails
+#define PXWIDTH 320  // Capture resolution, change this to match the output
+#define PXHEIGHT 240 // of `v4l2-ctl -d <n> --all | grep Bounds` if it fails
 
 // Global variables for options only
-int totalpixels = PXWIDTH*PXHEIGHT, minbright = 1, time = 5, fade = 500, oneshot_flag, help_flag;
+int totalpixels = PXWIDTH * PXHEIGHT, minbright = 1, time = 5, fade = 500,
+    oneshot_flag, help_flag;
 float multiplier = 1;
 char *device;
 
 float getbacklight() {
-	FILE *pipe = popen("xbacklight -get", "r");
-	char temp[16];
-	fscanf(pipe, "%s", temp);
-	float retval = atof(temp);
-	pclose(pipe);
-	return retval;
+    FILE *pipe = popen("xbacklight -get", "r");
+    char temp[16];
+    fscanf(pipe, "%s", temp);
+    float retval = atof(temp);
+    pclose(pipe);
+    return retval;
 }
 
 void setbacklight(float value, int time) {
-	char cmd[64];
-	sprintf(cmd, "xbacklight -set %f -time %i", value, time);
-	system(cmd);
+    char cmd[64];
+    sprintf(cmd, "xbacklight -set %f -time %i", value, time);
+    system(cmd);
 }
 
 // Get brightness of image from YUYV buffer
-float getbrightness(uint8_t* buffer) {
-	int bignumber = 0;
-	float returnval;
-	for (int i = 0; i < totalpixels; i+=2) {
-		bignumber+=buffer[i];
-	}
-	returnval = (bignumber/totalpixels - 8 + minbright) * multiplier; 
-	if (returnval <= 0) {
-		returnval = minbright;
-	}
-	return returnval;
+float getbrightness(uint8_t *buffer) {
+    int bignumber = 0;
+    float returnval;
+    for (int i = 0; i < totalpixels; i += 2) {
+        bignumber += buffer[i];
+    }
+    returnval = (bignumber / totalpixels - 8 + minbright) * multiplier;
+    if (returnval <= 0) {
+        returnval = minbright;
+    }
+    return returnval;
 }
 
 void help() {
-	printf("\
+    printf("\
 usage: xbacklight-auto [options]\n\
   options:\n\
     -h | --help                    display this help\n\
@@ -58,114 +58,114 @@ usage: xbacklight-auto [options]\n\
     -m | --minimum <percentage>    minimum possible brightness\n\
     -o | --oneshot                 run once and exit\n\
     -t | --time <seconds>          time between samples/sets (works best when ≥1)\n\
-    -x | --multiplier              multiplier on the set brightness (defaults to 1)\n\
-");
+    -x | --multiplier              multiplier on the set brightness (defaults to 1)\n"
+    );
 }
 
-void getoptions(int argc, char** argv) {
-	int c;
-	for (;;) {
-		static struct option long_options[] = 
-		{
-			{"help"			, no_argument, &help_flag	, 1},
-			{"oneshot"		, no_argument, &oneshot_flag, 1},
-			{"minimum"		, required_argument, 0, 'm'},
-			{"multiplier"	, required_argument, 0, 'x'},
-			{"time"			, required_argument, 0, 't'},
-			{"device"		, required_argument, 0, 'd'},
-			{"fade"			, required_argument, 0, 'f'},
-		};
+void getoptions(int argc, char **argv) {
+    int c;
+    for (;;) {
+        static struct option long_options[] = {
+            {"help", no_argument, &help_flag, 1},
+            {"oneshot", no_argument, &oneshot_flag, 1},
+            {"minimum", required_argument, 0, 'm'},
+            {"multiplier", required_argument, 0, 'x'},
+            {"time", required_argument, 0, 't'},
+            {"device", required_argument, 0, 'd'},
+            {"fade", required_argument, 0, 'f'},
+        };
 
-		int option_index = 0;
-		c = getopt_long(argc, argv, "m:t:x:ohf:", long_options, &option_index);
+        int option_index = 0;
+        c = getopt_long(argc, argv, "m:t:x:ohf:", long_options, &option_index);
 
-		if (c == -1) { break; }
+        if (c == -1) {
+            break;
+        }
 
-		switch(c) {
-			case 0:		//do nothing
-				break;
-			case 't': // time
-				time = atoi(optarg);
-				break;
-			case 'x': //multiplier
-				multiplier *=atof(optarg);
-				break;
-			case 'm': //minbright	
-				minbright = atoi(optarg);
-				break;
-			case 'o': // oneshot
-				oneshot_flag = 1;
-				break;
-			case 'd': // device
-				device = optarg;
-				break;
-			case 'f': // fade
-				fade = atoi(optarg);
-				break;
-			case 'h': // help 
-				help_flag = 1;
-				break;
-			default:
-				abort();
-		}
-	}
+        switch (c) {
+            case 0: // do nothing
+                break;
+            case 't': // time
+                time = atoi(optarg);
+                break;
+            case 'x': // multiplier
+                multiplier *= atof(optarg);
+                break;
+            case 'm': // minbright
+                minbright = atoi(optarg);
+                break;
+            case 'o': // oneshot
+                oneshot_flag = 1;
+                break;
+            case 'd': // device
+                device = optarg;
+                break;
+            case 'f': // fade
+                fade = atoi(optarg);
+                break;
+            case 'h': // help
+                help_flag = 1;
+                break;
+            default:
+                abort();
+        }
+    }
 }
 
 int main(int argc, char **argv) {
-	float brightness, xstate, offset = 1;
-	uint8_t *buffer;
-	int buf_addr;
-	int fd;
-	struct getbuf_return mybuffer;
+    float brightness, xstate, offset = 1;
+    uint8_t *buffer;
+    int buf_addr;
+    int fd;
+    struct getbuf_return mybuffer;
 
-	// Get options
-	getoptions(argc, argv);
+    // Get options
+    getoptions(argc, argv);
 
-	if (help_flag == 1) {
-		help();
-		return 0;
-	}
+    if (help_flag == 1) {
+        help();
+        return 0;
+    }
 
-	if (oneshot_flag == 1) {
-		time = 0;
-	}
+    if (oneshot_flag == 1) {
+        time = 0;
+    }
 
-	if (device == NULL) {
-		device = "/dev/video0";
-	}
-	
-	// main loop
-	do {
-		fd = camera_init(device, PXWIDTH, PXHEIGHT);
+    if (device == NULL) {
+        device = "/dev/video0";
+    }
 
-		if (fd != -1) {
-			mybuffer = get_buffer(fd);
-			
-			if (mybuffer.code != 0) {
-				camera_close(fd);
-				munmap(mybuffer.buffer, mybuffer.length);
-			} else {
-				camera_close(fd);
+    // main loop
+    do {
+        fd = camera_init(device, PXWIDTH, PXHEIGHT);
 
-				// get and set brightness
-				brightness = getbrightness(mybuffer.buffer)*offset;
-				setbacklight(brightness, fade);
+        if (fd != -1) {
+            mybuffer = get_buffer(fd);
 
-				// unmap v4l2 buffer from system memory
-				munmap(mybuffer.buffer, mybuffer.length);
-			}
-		}
-		sleep(time);
+            if (mybuffer.code != 0) {
+                camera_close(fd);
+                munmap(mybuffer.buffer, mybuffer.length);
+            } else {
+                camera_close(fd);
 
-		// get offset as a multiplier
-		xstate = getbacklight();
-		offset = xstate/(brightness/offset);
-	}	
-	while (oneshot_flag != 1);
+                // get and set brightness
+                brightness = getbrightness(mybuffer.buffer) * offset;
+                setbacklight(brightness, fade);
 
-	// close camera connection
-	int ret = camera_close(fd);
+                // unmap v4l2 buffer from system memory
+                munmap(mybuffer.buffer, mybuffer.length);
+            }
+        }
+        sleep(time);
 
-	// returns 0 if camera_close was successful, else -1
-	return ret;
+        // get offset as a multiplier
+        xstate = getbacklight();
+        offset = xstate / (brightness / offset);
+    } while (oneshot_flag != 1);
+
+    // close camera connection
+    int ret = camera_close(fd);
+
+    // returns 0 if camera_close was successful, else -1
+    return ret;
 }
